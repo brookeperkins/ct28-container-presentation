4b95e6d1ee3c8e67cf27d9a26d7444179f001230
brookeperkins <brookemmperkins@gmail.com>
2020-10-25 21:21:40 -0700

    single article passes, loading added

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Article from '../articles/Article';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <Article 
+    title="Title Here"
+    author="Nunya B. Usiness"
+    description="A great read, wow."
+  />;
 }
---
+++ src/components/articles/Article.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Article = ({ title, author, description }) => (
+  <>
+    <h3>{title}</h3>
+    <p>By: {author}</p>
+    <p>Summary: {description}</p>
+  </>
+);
+Article.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+export default Article;
